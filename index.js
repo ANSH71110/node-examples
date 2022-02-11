6c3a829444aa8b94af04a47ef399912ae9829eac
ANSH71110 <anshbale11@gmail.com>
2022-02-12 01:45:13 +0530

    node examples

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
+const rectangle = require('./rectangle');
 var rect=require('./rectangle');/*{
     perimeter:(x,y)=> (2*(x+y)),
     area:(x,y)=> (x*y)
 };*/
 
 function solveRect(l,b) {
-    console.log("Solving for rectangle with l="+l+" b="+b);
-    if(l<=0 || b<=0){
-        console.log("Rectangle dimensions should be greater than zero: l="+l+" b="+b);
-    }
-    else{
-        console.log("Area of Rectangle="+rect.area(l,b));
-        console.log("Perimeter of Rectangle="+rect.perimeter(l,b));
-    }
+    console.log("\nSolving for rectangle with l="+l+" b="+b);
+    rect(l,b,(err,rectangle)=>{
+        if(err){
+             console.log("ERROR: ",err.message);
+        }
+        else{
+            console.log("Area of Rectangle="+rectangle.area(l,b));
+            console.log("Perimeter of Rectangle="+rectangle.perimeter(l,b));
+        }
+    });
+    console.log("This statement is after the call to Rectangle");
 }
 
 solveRect(2,4);
